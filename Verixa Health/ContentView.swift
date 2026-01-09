744365f9c858f15694a21bd5a4194813122a2525
Kartik <kartikechoudhary111@gmail.com>
2026-01-09 10:58:03 +0530

    Initial Commit

---
+++ Verixa Health/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Verixa Health
+//
+//  Created by kartik choudhary  on 09/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Verixa Health/Verixa_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Verixa_HealthApp.swift
+//  Verixa Health
+//
+//  Created by kartik choudhary  on 09/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Verixa_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
